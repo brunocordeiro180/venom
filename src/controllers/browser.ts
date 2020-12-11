3091f09cd0cb1f277fb47462be8a6632b710bf85
Bruno Cordeiro <brunocordeiro180@gmail.com>
2020-12-11 11:50:53 -0300

    solve WAPI path to pkg

--- src/controllers/browser.ts
+++ src/controllers/browser.ts
@@ -110,15 +110,13 @@ export async function injectApi(page: Page) {
   });
 
   await page.addScriptTag({
-    path: require.resolve(path.join(__dirname, '../lib/wapi', 'wapi.js')),
+    path: path.join(process.cwd(), 'lib', 'wapi', 'wapi.js'),
   });
 
   await page.addScriptTag({
-    path: require.resolve(
-      path.join(__dirname, '../lib/middleware', 'middleware.js')
-    ),
+    path: path.join(process.cwd(), 'lib', 'middleware', 'middleware.js'),
   });
-
+  
   // Make sure WAPI is initialized
   await page.waitForFunction(() => {
     // @ts-ignore
